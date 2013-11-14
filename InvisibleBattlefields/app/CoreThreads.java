3dce7d69bb75c190574627523f78a14b326bf565
Rogério Carvalho Schneider <stockrt@gmail.com>
2013-11-14 01:44:56 -0200

    InvisibleBattlefieldsCoreServer

--- InvisibleBattlefields/app/CoreThreads.java
+++ InvisibleBattlefields/app/CoreThreads.java
@@ -1,4 +1,5 @@
 import br.pucrio.inf.lac.registry.RegistryCoreServer;
+import br.pucrio.inf.lac.invisiblebattlefields.InvisibleBattlefieldsCoreServer;
 
 public class CoreThreads {
 	private static class StartRegistryCoreServer implements Runnable {
@@ -7,11 +8,35 @@ public class CoreThreads {
 		}
 	}
 
+	private static class StartInvisibleBattlefieldsCoreServer implements
+			Runnable {
+		public void run() {
+			new InvisibleBattlefieldsCoreServer();
+		}
+	}
+
 	public static void initialize() {
-		StartRegistryCoreServer coreserver = new StartRegistryCoreServer();
-		Thread coreserver_thread = new Thread(coreserver);
-		System.out.print("Iniciando thread do RegistryCoreServer... ");
-		coreserver_thread.start();
-		System.out.println("Pronto.");
+		switch (System.getProperty("coreServer")) {
+		case "registry":
+			StartRegistryCoreServer registry_coreserver = new StartRegistryCoreServer();
+			Thread registry_coreserver_thread = new Thread(registry_coreserver);
+			System.out.print("Iniciando thread do RegistryCoreServer... ");
+			registry_coreserver_thread.start();
+			System.out.println("Pronto.");
+			break;
+		case "ibf":
+			StartInvisibleBattlefieldsCoreServer ibf_coreserver = new StartInvisibleBattlefieldsCoreServer();
+			Thread ibf_coreserver_thread = new Thread(ibf_coreserver);
+			System.out
+					.print("Iniciando thread do InvisibleBattlefieldsCoreServer... ");
+			ibf_coreserver_thread.start();
+			System.out.println("Pronto.");
+			break;
+		default:
+			System.out
+					.print("Nenhum CoreServer foi especificado, nenhuma thread adicional será instanciada... ");
+			System.out.println("Pronto.");
+			break;
+		}
 	}
 }
