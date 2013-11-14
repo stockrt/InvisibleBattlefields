3c7355308f0df6bda61a3d7bd2fd1ff2662472f4
Rogério Carvalho Schneider <stockrt@gmail.com>
2013-11-14 01:23:56 -0200

    Indent

--- InvisibleBattlefields/app/Global.java
+++ InvisibleBattlefields/app/Global.java
@@ -1,8 +1,8 @@
 import play.*;
 
 public class Global extends GlobalSettings {
-    public void onStart(Application app) {
-        System.out.println("Inicializando threads...");
-        CoreThreads.initialize();
-    }
+	public void onStart(Application app) {
+		System.out.println("Inicializando threads...");
+		CoreThreads.initialize();
+	}
 }
