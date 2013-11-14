020186422d252ec0b5f138aa26323d3c5d5ec7bb
Rogério Carvalho Schneider <stockrt@gmail.com>
2013-11-13 23:21:23 -0200

    Mais de um Core Server

--- provision/core-server.sh
+++ provision/core-server.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 source /config.sh
-pkill -f "$HOME/play-$PLAY_VERSION"
-sleep 2
-rm -f $REPOSITORY_HOME/InvisibleBattlefields/RUNNING_PID
-cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play $@
+
+if [[ "$1" == "registry" ]]
+then
+    pkill -f "DcoreServer=registry"
+    sleep 2
+    rm -f $REPOSITORY_HOME/InvisibleBattlefields/registry.pid
+    cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play -DcoreServer=registry -Dpidfile.path=$REPOSITORY_HOME/InvisibleBattlefields/registry.pid -Dhttp.port=9000 $@
+elif [[ "$1" == "ibf" ]]
+then
+    pkill -f "DcoreServer=ibf"
+    sleep 2
+    rm -f $REPOSITORY_HOME/InvisibleBattlefields/ibf.pid
+    cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play -DcoreServer=ibf -Dpidfile.path=$REPOSITORY_HOME/InvisibleBattlefields/ibf.pid -Dhttp.port=9001 $@
+else
+    cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play $@
+fi
