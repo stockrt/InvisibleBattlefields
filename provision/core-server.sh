0ec496d8a4b8757d6cb57dd6278eb77cb7500065
Rogério Carvalho Schneider <stockrt@gmail.com>
2013-11-13 23:25:30 -0200

    pidfile

--- provision/core-server.sh
+++ provision/core-server.sh
@@ -4,16 +4,18 @@ source /config.sh
 
 if [[ "$1" == "registry" ]]
 then
+    pidfile="$REPOSITORY_HOME/InvisibleBattlefields/registry.pid"
     pkill -f "DcoreServer=registry"
     sleep 2
-    rm -f $REPOSITORY_HOME/InvisibleBattlefields/registry.pid
-    cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play -DcoreServer=registry -Dpidfile.path=$REPOSITORY_HOME/InvisibleBattlefields/registry.pid -Dhttp.port=9000 $@
+    rm -f $pidfile
+    cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play -DcoreServer=registry -Dpidfile.path=$pidfile -Dhttp.port=9000 $@
 elif [[ "$1" == "ibf" ]]
 then
+    pidfile="$REPOSITORY_HOME/InvisibleBattlefields/ibf.pid"
     pkill -f "DcoreServer=ibf"
     sleep 2
-    rm -f $REPOSITORY_HOME/InvisibleBattlefields/ibf.pid
-    cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play -DcoreServer=ibf -Dpidfile.path=$REPOSITORY_HOME/InvisibleBattlefields/ibf.pid -Dhttp.port=9001 $@
+    rm -f $pidfile
+    cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play -DcoreServer=ibf -Dpidfile.path=$pidfile -Dhttp.port=9001 $@
 else
     cd $REPOSITORY_HOME/InvisibleBattlefields && $HOME/play-$PLAY_VERSION/play $@
 fi
